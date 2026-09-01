473e1c76a2b9dad3671769f0890a230a03274afe
silviarosa00 <silvia.rosa@student.unibz.it>
2026-09-01 15:28:46 +0200

    Update paths and job parameters in FastQC script

--- bioinformatic_pipeline/03_fastqc_post-sickle.sh
+++ bioinformatic_pipeline/03_fastqc_post-sickle.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=fastqc_post_sickle
 #SBATCH --ntasks=1
 #SBATCH --mem=16G
-#SBATCH --partition=bru-medium
-#SBATCH --account=brusetti-lab
+#SBATCH --partition= ##add
+#SBATCH --account=##add
 #SBATCH --time=04:00:00
 #SBATCH --output=fastqc-%j.out
 #SBATCH --error=fastqc-%j.err
@@ -11,8 +11,8 @@
 # === ATTIVA CONDA se serve ===
 module load fastqc/0.12.1-gcc-12.1.0
 
-INPUT_DIR="/data/nanopore01/subsurfice/T2/DATA/Raw_data/sickle_out/"
-OUTPUT_DIR="/data/nanopore01/subsurfice/T2/DATA/Raw_data/sickle_out/fastqc_results"
+INPUT_DIR="/your/path/sickle_out/"
+OUTPUT_DIR="your/path/sickle_out/fastqc_results"
 mkdir -p "$OUTPUT_DIR"
 
 for fq in "$INPUT_DIR"/*.fastq; do
@@ -21,4 +21,4 @@ for fq in "$INPUT_DIR"/*.fastq; do
     fastqc "$fq" -o "$OUTPUT_DIR"
 done
 
-echo "[✅] FastQC post-Sickle completato."
+echo "[✅] FastQC post-Sickle complete."
